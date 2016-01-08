d290282a53855e8106f046137c30149289e1e0df
H. İbrahim Güngör <igungor@gmail.com>
2016-01-08 22:08:33 +0200

    all: more granular file structure

---
+++ pfft/game.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/igungor/quackle"
+)
+
+const (
+	lexicon  = "turkish"
+	alphabet = "turkish"
+	datadir  = "data"
+)
+
+var game quackle.Game
+
+// hacky stuff.
+// var datadir = fmt.Sprintf("%v/src/github.com/igungor/cmd/pfft/data", os.Getenv("GOPATH"))
+
+func initGame() {
+	// set up data manager
+	dm := quackle.NewDataManager()
+	dm.SetComputerPlayers(quackle.ComputerPlayerCollectionFullCollection().SwigGetPlayerList())
+	dm.SetBackupLexicon(lexicon)
+	dm.SetAppDataDirectory(datadir)
+
+	// set up alphabet
+	abc := quackle.AlphabetParametersFindAlphabetFile(alphabet)
+	qabc := quackle.UtilStdStringToQString(abc)
+	flexAbc := quackle.NewFlexibleAlphabetParameters()
+	flexAbc.Load(qabc)
+	dm.SetAlphabetParameters(flexAbc)
+
+	// set up board
+	board := quackle.NewBoardParameters()
+	for y := 0; y < boardsize; y++ {
+		for x := 0; x < boardsize; x++ {
+			board.SetLetterMultiplier(x, y, quackle.QuackleBoardParametersLetterMultiplier(boardLetterMult[x][y]))
+			board.SetWordMultiplier(x, y, quackle.QuackleBoardParametersWordMultiplier(boardWordMult[x][y]))
+		}
+	}
+	dm.SetBoardParameters(board)
+
+	// find lexicon
+	dawg := quackle.LexiconParametersFindDictionaryFile(lexicon + ".dawg")
+	gaddag := quackle.LexiconParametersFindDictionaryFile(lexicon + ".gaddag")
+	dm.LexiconParameters().LoadDawg(dawg)
+	dm.LexiconParameters().LoadGaddag(gaddag)
+	dm.StrategyParameters().Initialize(lexicon)
+
+	dm.SeedRandomNumbers(42)
+
+	// set up players and game
+	game := quackle.NewGame()
+	player1, _ := newComputerPlayer(dm, "comp1", 0)
+	player2 := newHumanPlayer(dm, "iby", 1)
+	players := quackle.NewPlayerList()
+	players.Add(player1)
+	players.Add(player2)
+	game.SetPlayers(players)
+	game.AssociateKnownComputerPlayers()
+	game.AddPosition()
+}
+
+func getCurPos(game quackle.Game) quackle.GamePosition {
+	return game.CurrentPosition().(quackle.GamePosition)
+}
+
+func newComputerPlayer(dm quackle.DataManager, name string, id int) (quackle.Player, error) {
+	var player quackle.Player
+	found := make([]bool, 1)
+	player = dm.ComputerPlayers().PlayerForName("Speedy Player", found)
+	if !found[0] {
+		return player, fmt.Errorf("player could not be found")
+	}
+	comp := player.ComputerPlayer()
+
+	player = quackle.NewPlayer(name)
+	player.SetComputerPlayer(comp)
+	player.SetType(int(quackle.PlayerComputerPlayerType))
+	player.SetId(id)
+	return player, nil
+}
+
+func newHumanPlayer(dm quackle.DataManager, name string, id int) quackle.Player {
+	player := quackle.NewPlayer(name)
+	player.SetType(int(quackle.PlayerHumanPlayerType))
+	player.SetId(id)
+	return player
+}
--- pfft/main.go
+++ pfft/main.go
@@ -3,10 +3,19 @@ package main
 // TODO(ig): draw board on the center. positions are hardcoded currently.
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"os"
 	"strconv"
+	"strings"
 
 	"github.com/nsf/termbox-go"
+)
+
+const (
+	fgcolor = termbox.ColorDefault
+	bgcolor = termbox.ColorDefault
 )
 
 func main() {
@@ -21,6 +30,8 @@ func realMain() error {
 	}
 	defer termbox.Close()
 	termbox.SetInputMode(termbox.InputEsc)
+
+	initGame()
 
 	drawBoard(110, 25, 15, 15)
 	termbox.Flush()
@@ -43,6 +54,8 @@ mainloop:
 
 // drawBoard draws the board onto (x,y) position of the grid.
 func drawBoard(x, y, w, h int) {
+	sx, sy := termbox.Size()
+	fill(0, 0, sx, sy, ' ')
 	// columns on the top
 	for dx := 0; dx < w; dx++ {
 		termbox.SetCell(x+1+dx*2, y-2, rune('A'+dx), fgcolor, bgcolor)
@@ -94,7 +107,62 @@ func fill(x, y, w, h int, r rune) {
 	}
 }
 
-const (
-	fgcolor = termbox.ColorDefault
-	bgcolor = termbox.ColorDefault
-)
+// func move() {
+// 	// play
+// 	for {
+// 		if getCurPos(game).GameOver() {
+// 			fmt.Println("GAME OVER!")
+// 			break
+// 		}
+// 		player := getCurPos(game).CurrentPlayer().(quackle.Player)
+
+// 		// go on cold heartless machine!
+// 		if quackle.QuacklePlayerPlayerType(player.Xtype()) == quackle.PlayerComputerPlayerType {
+// 			move := game.HaveComputerPlay()
+// 			fmt.Println("Rack: ", player.Rack().ToString())
+// 			fmt.Println("Move: ", move.ToString())
+// 			fmt.Printf("Board:\n %v\n", getCurPos(game).Board().ToString())
+// 			continue
+// 		}
+
+// 		game.AdvanceToNoncomputerPlayer()
+// 		fmt.Println("Rack: ", player.Rack().ToString())
+
+// 		// read input
+// 		var move quackle.Move
+// 	MOVELOOP:
+// 		for {
+// 			r := bufio.NewReader(os.Stdin)
+// 			input, _ := r.ReadString('\n')
+// 			input = strings.TrimSuffix(input, "\n")
+// 			fields := strings.Fields(input)
+// 			switch len(fields) {
+// 			case 1:
+// 				// pass
+// 				if fields[0] == "-" {
+// 					move = quackle.MoveCreatePassMove()
+// 					game.CommitMove(move)
+// 					break MOVELOOP
+// 				}
+// 				fmt.Println("NEIN! gecerli biseyler yaz")
+// 				continue MOVELOOP
+// 			case 2:
+// 				place, word := fields[0], fields[1]
+// 				move = quackle.MoveCreatePlaceMove(place, dm.AlphabetParameters().Encode(word))
+// 				if getCurPos(game).ValidateMove(move) == int(quackle.GamePositionValidMove) {
+// 					game.CommitMove(move)
+// 					break MOVELOOP
+// 				}
+// 				fmt.Println("NEIN! gecerli bir hamle degil")
+// 				continue MOVELOOP
+// 			default:
+// 				fmt.Println("NEIN! gecerli biseyler yaz")
+// 				continue MOVELOOP
+// 			}
+// 		}
+
+// 		fmt.Println("Rack: ", player.Rack().ToString())
+// 		fmt.Println("Move: ", move.ToString())
+// 		fmt.Printf("Board:\n %v\n", getCurPos(game).Board().ToString())
+// 	}
+// }
