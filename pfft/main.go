f70a49b2376adc424c41f808d72a357cc1728c34
H. İbrahim Güngör <igungor@gmail.com>
2016-01-11 00:33:33 +0200

    pfft: remove debug codes

--- pfft/main.go
+++ pfft/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"log"
-	"os"
 
 	termbox "github.com/nsf/termbox-go"
 )
-
-var f *os.File
 
 func main() {
 	if err := realMain(); err != nil {
@@ -21,13 +18,6 @@ func realMain() error {
 	}
 	defer termbox.Close()
 	termbox.SetInputMode(termbox.InputEsc)
-
-	var err error
-	f, err = os.OpenFile("foo", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
 
 	game := newGame()
 	game.draw()
