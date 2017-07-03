58cb43850af6bf453cba0e994ea087d8f973d56a
H. İbrahim Güngör <igungor@gmail.com>
2017-07-03 17:22:05 +0300

    gos: new command
    search godoc.org from command line.

---
+++ gos/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+const godocURL = "https://api.godoc.org/search?q="
+
+func main() {
+	log.SetFlags(0)
+
+	// flags
+	var (
+		flagCount = flag.Int("n", 5, "number of results")
+	)
+
+	flag.Usage = usage
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		flag.Usage()
+	}
+
+	client := &http.Client{Timeout: 20 * time.Second}
+
+	query := flag.Arg(0)
+	resp, err := client.Get(godocURL + url.QueryEscape(query))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal("not ok")
+	}
+
+	var r result
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(r.Results) == 0 {
+		log.Fatal("no result found")
+	}
+
+	for _, result := range r.Results[:*flagCount] {
+		fmt.Printf("\n\033[1m%v\033[m\n", result.Path)
+		syn := result.Synopsis
+		if syn != "" {
+			fmt.Printf("  %v\n", syn)
+		}
+	}
+}
+
+type result struct {
+	Results []struct {
+		Path     string
+		Synopsis string
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage:\n")
+	fmt.Fprintf(os.Stderr, "  gos [query]\n")
+	flag.PrintDefaults()
+	os.Exit(1)
+}
