fabf37316bd7680fd6746458cbb78e8deb1e3065
H. İbrahim Güngör <igungor@gmail.com>
2017-09-25 10:37:27 +0300

    test-server: add a counter option

--- test-server/main.go
+++ test-server/main.go
@@ -7,13 +7,15 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"strconv"
+	"sync"
 )
 
 func main() {
 	var (
-		flagDumpRequest = flag.Bool("d", false, "dump full request to stdout")
-		flagLogRequest  = flag.Bool("l", false, "dump request path to stdout")
-		flagPort        = flag.Uint("p", 0, "port to listen to")
+		flagDumpRequest  = flag.Bool("d", false, "dump full request to stdout")
+		flagLogRequest   = flag.Bool("l", false, "dump request path to stdout")
+		flagCountRequest = flag.Bool("c", false, "count requests")
+		flagPort         = flag.Uint("p", 0, "port to listen to")
 	)
 	flag.Parse()
 	ln, err := net.Listen("tcp", net.JoinHostPort("0.0.0.0", strconv.Itoa(int(*flagPort))))
@@ -22,6 +24,10 @@ func main() {
 	}
 	log.Printf("Running on %v\n", ln.Addr())
 
+	var (
+		mu      sync.Mutex
+		counter int64
+	)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if *flagLogRequest {
 			log.Printf("%v\n", r.URL)
@@ -29,6 +35,13 @@ func main() {
 		if *flagDumpRequest {
 			b, _ := httputil.DumpRequest(r, true)
 			log.Println(string(b))
+		}
+
+		if *flagCountRequest {
+			mu.Lock()
+			counter++
+			mu.Unlock()
+			log.Printf("counter: %d\n", counter)
 		}
 	})
 	log.Fatal(http.Serve(ln, nil))
