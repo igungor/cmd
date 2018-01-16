431e7dc96b7878541afbd3a527a1934e05bd0620
H. İbrahim Güngör <igungor@gmail.com>
2018-01-16 17:56:09 +0300

    avg: parse date as well (best effort)

--- avg/main.go
+++ avg/main.go
@@ -26,6 +26,10 @@ func main() {
 		count int64
 
 		firsttype typ
+
+		// date parsing
+		lastdate   time.Time
+		datelayout string
 	)
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
@@ -49,6 +53,19 @@ func main() {
 			f, _ := strconv.ParseFloat(line, 64)
 			total += f
 			count++
+		case date:
+			if lastdate.IsZero() {
+				lastdate, datelayout = layout(line)
+				continue
+			}
+
+			t, err := time.Parse(datelayout, line)
+			if err != nil {
+				log.Fatalf("could not parse %q with layout %q", line, datelayout)
+			}
+			total += float64(t.Sub(lastdate).Nanoseconds())
+			count++
+			lastdate = t
 		case unknown:
 			log.Fatalf("unrecognized input %q", line)
 		}
@@ -65,6 +82,8 @@ func main() {
 		fmt.Println(int64(total) / count)
 	case float:
 		fmt.Println(total / float64(count))
+	case date:
+		fmt.Println((time.Duration(total) / time.Duration(count)).String())
 	}
 }
 
@@ -76,6 +95,7 @@ const (
 	duration
 	integer
 	float
+	date
 )
 
 func typeof(s string) typ {
@@ -91,5 +111,25 @@ func typeof(s string) typ {
 	if err == nil {
 		return float
 	}
+	_, layout := layout(s)
+	if layout != "" {
+		return date
+	}
 	return unknown
+}
+
+func layout(s string) (time.Time, string) {
+	for _, layout := range layouts {
+		t, err := time.Parse(layout, s)
+		if err == nil {
+			return t, layout
+		}
+	}
+	return time.Time{}, ""
+}
+
+var layouts = []string{
+	"2006-01-02 15:04:05.999",
+	time.RFC3339,
+	time.RFC3339Nano,
 }
