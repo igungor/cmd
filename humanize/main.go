e26476e13da708c23bb8ff9b1db894e6d43daacc
H. İbrahim Güngör <igungor@gmail.com>
2018-05-28 14:14:55 +0300

    cmd: add humanize

---
+++ humanize/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	humanizepkg "github.com/dustin/go-humanize"
+)
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		s := bufio.NewScanner(os.Stdin)
+		for s.Scan() {
+			fmt.Println(s.Text())
+		}
+		if err := s.Err(); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	for _, arg := range flag.Args() {
+		fmt.Println(humanize(arg))
+	}
+
+}
+
+func humanize(s string) string {
+	i, err := strconv.ParseUint(s, 10, 64)
+	if err != nil {
+		log.Fatalf("could not parse %v: %v", s, err)
+	}
+	return humanizepkg.Bytes(i)
+}
