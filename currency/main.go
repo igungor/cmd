905c31422538b425620c4071e706c1eb0d5186c7
İbrahim Güngör <igungor@gmail.com>
2021-09-29 09:59:05 +0300

    cmd/currency: use yahoo api

--- currency/main.go
+++ currency/main.go
@@ -1,200 +1,97 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
+	"net/url"
 	"os"
-	"path/filepath"
-	"strings"
 	"time"
 )
 
-const (
-	userAgent   = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
-	timeLayout  = "02/01/2006"
-	storagePath = ".local/share"
-)
-
 func main() {
-	flag.Parse()
-
-	currencies, err := GetCurrencies(flag.Args()...)
-	if err != nil {
-		log.Fatal(err)
+	if err := realmain(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-
-	fmt.Println(prettyPrint(currencies...))
 }
 
-func GetCurrencies(codes ...string) ([]Currency, error) {
-	c := &http.Client{Timeout: 10 * time.Second}
+func realmain() error {
+	fmt.Println(request("EUR"))
 
-	const baseurl = "https://www.doviz.com/api/v1/currencies/all/latest"
+	return nil
+}
 
-	req, err := http.NewRequest("GET", baseurl, nil)
+func request(code string) (float64, error) {
+	const to = "TRY=X"
+
+	u, _ := url.Parse(yahooFinanceURL)
+	u.Path += fmt.Sprintf("%v%v", code, to)
+	params := u.Query()
+	params.Set("range", "1d")
+	u.RawQuery = params.Encode()
+
+	c := http.Client{Timeout: time.Minute}
+
+	resp, err := c.Get(u.String())
 	if err != nil {
-		return nil, fmt.Errorf("could not create request: %v", err)
-	}
-	req.Header.Set("X-Requested-With", "XMLHttpRequest")
-
-	resp, err := c.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("could not fetch currencies: %v", err)
+		return 0, fmt.Errorf("yahoo: could not fetch response: %v", err)
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("unexpected HTTP status code: %v", resp.StatusCode)
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("yahoo: unexpected status code %v", resp.StatusCode)
 	}
 
-	var currencies []Currency
-	if err := json.NewDecoder(resp.Body).Decode(&currencies); err != nil {
-		return nil, fmt.Errorf("could not decode json: %v", err)
+	var response yahooFinanceResponse
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return 0, fmt.Errorf("yahoo: parse json: %v", err)
 	}
 
-	if len(codes) == 0 {
-		return currencies, nil
-	}
+	result := response.Chart.Result
+	quote := result[len(result)-1].Indicators.Quote
+	closed := quote[len(quote)-1].Close
+	prevClose := result[len(result)-1].Meta.PreviousClose
 
-	var selection []Currency
-	for _, curr := range currencies {
-		currcode := strings.ToLower(curr.Code)
-		for _, code := range codes {
-			code = strings.ToLower(code)
-			if currcode == code {
-				selection = append(selection, curr)
-			}
+	if len(closed) == 0 {
+		// some currencies are not available in 'close data', such as BGN.
+		// dont let people down.
+		if prevClose != 0 {
+			return prevClose, nil
 		}
-
+		return 0, fmt.Errorf("yahoo: no value found for code %q", code)
 	}
-	return selection, nil
-}
 
-type Currency struct {
-	Selling    float64 `json:"selling"`
-	UpdateDate float64 `json:"update_date"`
-	Currency   int     `json:"currency"`
-	Buying     float64 `json:"buying"`
-	ChangeRate float64 `json:"change_rate"`
-	Name       string  `json:"name"`
-	FullName   string  `json:"full_name"`
-	Code       string  `json:"code"`
-}
-
-func prettyPrint(currencies ...Currency) string {
-	const (
-		boldblack = "\x1b[30m"
-		black     = "\x1b[1;30m"
-		red       = "\x1b[35m"
-		green     = "\x1b[32m"
-		reset     = "\x1b[0m"
-	)
-	color := func(f float64) string {
-		switch {
-		case f < 0:
-			return "red"
-		case f > 0:
-			return "green"
-		default:
-			return "black"
+	var rates []float64
+	for _, v := range closed {
+		rate, ok := v.(float64)
+		// skip unrecognized values to a list for later use
+		if !ok {
+			continue
 		}
+		rates = append(rates, rate)
 	}
-
-	var buf bytes.Buffer
-
-	for _, c := range currencies {
-		code := strings.ToUpper(c.Code)
-
-		sethop(code, time.Now(), c.ChangeRate)
-
-		// calculate the fall
-		hop := gethop(code)
-		var freefall string
-		for _, h := range hop {
-			if h.DailyChange < 0 {
-				freefall += "◉ "
-			} else {
-				freefall = ""
-			}
-		}
-
-		fmt.Fprintf(&buf, "%v <%.2f%%> \x1b[31;1;8m%v\x1b[0m | size=13 color=%v href=https://tr.investing.com/currencies/%v-try-commentary\n", code, c.ChangeRate, freefall, color(c.ChangeRate), strings.ToLower(code))
-		fmt.Fprintf(&buf, "• Al: %.4f | size=11 color=black\n", c.Buying)
-		fmt.Fprintf(&buf, "• Sat: %.4f | size=11 color=black\n", c.Selling)
-		fmt.Fprintf(&buf, "---\n")
-	}
-	return buf.String()
+	return rates[len(rates)-1], nil
 }
 
-func sethop(code string, date time.Time, change float64) {
-	home := os.Getenv("HOME")
-	path := filepath.Join(home, storagePath)
-	os.MkdirAll(path, 0755)
-	fpath := filepath.Join(path, "currency.json")
-
-	_, err := os.Stat(fpath)
-	if os.IsNotExist(err) {
-		ioutil.WriteFile(fpath, []byte("{}"), 0644)
-	}
-
-	allhops := gethops()
-
-	datestr := date.Format(timeLayout)
-	hop := Hop{Date: datestr, DailyChange: change}
-
-	hops, ok := allhops[code]
-	if !ok {
-		hops = []Hop{hop}
-	} else {
-		var found bool
-		for _, h := range hops {
-			if h.Date == datestr {
-				found = true
-				break
-			}
-		}
-		if !found {
-			hops = append(hops, hop)
-		}
-	}
-
-	// limit the size of the slice to 3
-	if len(hops) >= 3 {
-		hops = hops[len(hops)-3 : len(hops)]
-	}
-
-	allhops[code] = hops
-
-	b, _ := json.Marshal(allhops)
-	_ = ioutil.WriteFile(fpath, b, 0644)
-}
-
-func gethops() map[string][]Hop {
-	home := os.Getenv("HOME")
-	path := filepath.Join(home, storagePath)
-	os.MkdirAll(path, 0755)
-	fpath := filepath.Join(path, "currency.json")
-
-	m := make(map[string][]Hop)
-
-	content, _ := ioutil.ReadFile(fpath)
-	_ = json.Unmarshal(content, &m)
-
-	return m
-}
-
-func gethop(code string) []Hop {
-	all := gethops()
-	v, _ := all[code]
-	return v
-}
-
-type Hop struct {
-	Date        string
-	DailyChange float64
+type yahooFinanceResponse struct {
+	Chart struct {
+		Error  interface{} `json:"error"`
+		Result []struct {
+			Timestamp  []int64 `json:"timestamp"`
+			Indicators struct {
+				Quote []struct {
+					Close  []interface{} `json:"close"`
+					High   []interface{} `json:"high"`
+					Low    []interface{} `json:"low"`
+					Open   []interface{} `json:"open"`
+					Volume []interface{} `json:"volume"`
+				} `json:"quote"`
+			} `json:"indicators"`
+			Meta struct {
+				PreviousClose float64 `json:"previousClose"`
+			} `json:"meta"`
+		} `json:"result"`
+	} `json:"chart"`
 }
